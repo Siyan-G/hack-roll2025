92011cb437bf83baad6f2fbdc1e2001b7bfd1178
Siyan-G <gsy7418@gmail.com>
2025-01-18 12:37:29 +0800

    set up frontend folder

---
+++ frontend/src/components/input/uploadcontainer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, Typography, Container } from '@mui/material';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/system';
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+export default function UploadContainer() {
+
+    return (
+      <Container fixed={true} sx={{ bgcolor: "lightyellow", height: "100vh" }}>
+        <Button
+          component="label"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<CloudUploadIcon />}
+        >
+          Upload files
+          <VisuallyHiddenInput
+            type="file"
+            onChange={(event) => console.log(event.target.files)}
+            multiple
+          />
+        </Button>
+      </Container>
+    );
+}
